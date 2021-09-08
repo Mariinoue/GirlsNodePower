98673f8345d22b9e12efe3d2b4467242cbd0390d
Eilem <oi.eilem@gmail.com>
2021-09-07 22:54:12 -0300

    teste de conexao com banco

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ module.exports={
     host:'localhost',
     port:5432,
     database:'GirlsNodePower_db',
+    // database:'teste_node', //apenas testando criaçaõ de tabelas em outro banco
     username: 'postgres',
     password:'root'
-
 }
