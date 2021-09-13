22875db623cbdde24d24cb545aaf6cbc333476f3
AriadnaSpineli <ariadna.spin@yahoo.com.br>
2021-09-13 13:26:23 -0300

    alterando a porta do banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     dialect: 'postgres',
     host:'localhost',
-    port:3320,
+    port:5432,
     database:'GirlsNodePower_db',
     username: 'postgres',
     password:'root'
